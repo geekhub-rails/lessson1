439381eeee8ea654d8fdd72727142bc19ac8a798
Bohdan Prokopenko <viking300kg@vu.cdu.edu.ua>
2018-10-15 23:47:44 +0300

    finished metod sum, started method age

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,25 @@
 require 'date'
 class Lesson1
-  def sum(val = 0)
-    # TODO
-  end
+	def sum(val = 0)
+		string = val.to_s
+		arr = string.split ''
+		int_arr = Array.new
+		arr.each do |x|
+			int_arr.push x.to_i
+		end
+		int_arr.inject(0) {|res, el| res + el}
+	end
 
-  def age(birthday)
-    # TODO
-  end
+	def age(birthday)
+		require 'date'
+		require 'time'
+		birth_date = DateTime.parse birthday
+		yearas_old = DateTime.now.year - birthday.year
 
-  def name
-    # TODO
-  end
+
+	end
+
+	def name
+		# TODO
+	end
 end
