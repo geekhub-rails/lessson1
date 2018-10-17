73982a5634608c24b58521e3e0827f34ed5e3497
somenickname <just.leks@gmail.com>
2018-10-17 22:06:19 +0300

    make homework

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,30 @@
 require 'date'
 class Lesson1
   def sum(val = 0)
-    # TODO
+    val.to_s.chars.map(&:to_i).reduce {|sum, char| sum += char}
   end
 
   def age(birthday)
-    # TODO
+    return 'Invalid Date Format' unless birthday
+
+    date = Date.strptime(birthday, '%d/%m/%Y')
+    birth = Date.civil(date.year, date.month, date.day)
+    now = Date.today
+
+    years = (now.year - birth.year).to_s
+    days = now - birth
+    hours = days * 24
+    minutes = hours * 60
+    seconds = minutes * 60
+
+    "Я живу #{forever_young(years)} года или #{days.to_i} дней или #{hours.to_i} часов или #{minutes.to_i} минут или #{seconds.to_i} секунд"
   end
 
   def name
-    # TODO
+    "Hello #{gets.chomp} #{gets.chomp} #{gets.chomp}!"
+  end
+
+  def forever_young(years)
+    years.to_i - 3
   end
 end
